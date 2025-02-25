ff8aca5f1562358a3a3e7e037da556791609a06a
Viktor Gaponenko <vikt.gaponenko@gmail.com>
2025-02-25 22:30:55 +0100

    fix(GuidedPayload): ConfigurableJoint configuration

--- Runtime/Scripts/MaterialFlow/GuidedPayload.cs
+++ Runtime/Scripts/MaterialFlow/GuidedPayload.cs
@@ -143,7 +143,11 @@ namespace OC.MaterialFlow
             _joint.yMotion = ConfigurableJointMotion.Locked;
             _joint.zMotion = ConfigurableJointMotion.Locked;
             _joint.angularXMotion = ConfigurableJointMotion.Locked;
+#if UNITY_6_0_OR_NEWER
+            _joint.angularYMotion = ConfigurableJointMotion.Free;
+#else
             _joint.angularYMotion = ConfigurableJointMotion.Locked;
+#endif
             _joint.angularZMotion = ConfigurableJointMotion.Locked;
         }
 
@@ -156,7 +160,7 @@ namespace OC.MaterialFlow
             _joint.connectedAnchor =  _transport.GetClosetPoint(_transform.position);
             _rigidbody.transform.rotation = Quaternion.LookRotation(normal, Vector3.up) * Quaternion.AngleAxis(_angleOffset, Vector3.up);
             _joint.axis = Quaternion.AngleAxis(_angleOffset, Vector3.up) * Vector3.forward;
-            _rigidbody.velocity = normal * _transport.Value.Value;
+            if (!_rigidbody.isKinematic) _rigidbody.velocity = normal * _transport.Value.Value;
         }
 
         private float GetOffsetAngle(Transport transport)
